7cd7f545d96456be1e71ad1f52e5ab5d5bd80568
aalenezi1 <aalenezi1@mail.csuchico.edu>
2018-02-24 00:07:51 -0800

    final work

--- main.cpp
+++ main.cpp
@@ -7,13 +7,28 @@ int main()
   string list[5]; //array of 5 strings
   int numItems = 0;
   char input;
-
-  cout<<"\n==GROCERY LIST MANAGER==";
+  string items;
+ do
+{
+cout<<"\n==GROCERY LIST MANAGER==";
   cout<<"\nEnter your choice: ";
   cout<<"\n (A)dd an item";
   cout<<"\n (Q)uit";
   cout<<"\nYour choice (A/Q): ";
   cin>>input;
+    if(( input == 'a' ) || ( input == 'A'))
+{
+cout<<"what is the item?\n";
+ cin>>items;
+ numItems++;
+ if( numItems >= 6 )
+ {
 
-  return 0;
+ cout<< "You'll need a bigger list!\n";
+
+   }
+  }
+}
+while(( input!='q' ) && ( input!='q' ));
+ return 0;
 }
